04816cfca013921a5ea2aaefb8bf52cdaea12080
Michael Henretty <michael.henretty@gmail.com>
2018-05-09 12:16:31 +0200

    add nodejs script for generating empty corpus.csv file

---
+++ makeEmptyCSV.js
@@ -0,0 +1,15 @@
+const LINES_IN_CROWDIN = 200000;
+const fs = require('fs');
+
+const OUTPUT_PATH = './corpus.csv';
+
+let output = '';
+for (var i = 1; i <= LINES_IN_CROWDIN; i++) {
+  output += `"${i}",""\n`;
+}
+
+fs.writeFile(OUTPUT_PATH, output, (err) => {
+  if (err) {
+    console.error('error', err);
+  }
+});
